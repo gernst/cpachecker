a01abd56e17b5c060a11327cee8ae743a489c9bb
Christoph Girstenbrei <Christoph.Girstenbrei@gmail.com>
2020-07-17 08:58:52 +0000

    Assignment of known values in ExpressionValueVisitor

--- src/org/sosy_lab/cpachecker/cpa/value/AbstractExpressionValueVisitor.java
+++ src/org/sosy_lab/cpachecker/cpa/value/AbstractExpressionValueVisitor.java
@@ -797,6 +797,8 @@ public abstract class AbstractExpressionValueVisitor
     CExpression functionNameExp = pIastFunctionCallExpression.getFunctionNameExpression();
 
     // We only handle builtin functions
+    // TODO christoph kucken ob calledFunctionName == nondet_int
+    // Wenn es einer von denen
     if (functionNameExp instanceof CIdExpression) {
       String calledFunctionName = ((CIdExpression) functionNameExp).getName();
 
